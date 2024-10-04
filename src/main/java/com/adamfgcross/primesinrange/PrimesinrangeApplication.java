69a1cff5befbbb01d811df84e2039c88f2d468f5
Adam <adamfgcross@gmail.com>
2024-10-04 11:11:55 -0400

    initial commit

---
+++ src/main/java/com/adamfgcross/primesinrange/PrimesinrangeApplication.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.primesinrange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimesinrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimesinrangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adamfgcross/primesinrange/PrimesinrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfgcross.primesinrange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimesinrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
